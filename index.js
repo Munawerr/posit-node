bd297e118cc454f61f912444c85d5d89faacba1a
Munawerr <munaweranwar2@gmail.com>
2020-10-28 14:37:40 +0500

    Reshuffeled the code

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const app = express();
 // const mongoose = require("mongoose");
+const port = process.env.PORT || 8080;
 
 //import routes
 const restRoutes = require("./routes/restaurants");
 
 app.use("/", restRoutes);
 
-
-const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listenting to Port ${port}...`));
