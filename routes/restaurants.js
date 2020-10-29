d55aab6f2d8c3cd866ede82fff20a19f8fc2269d
Munawerr <munaweranwar2@gmail.com>
2020-10-29 12:03:06 +0500

    reshuffle

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -16,6 +16,10 @@ const schema = Joi.object({
   RType: Joi.string().min(2).max(2),
 });
 router.get("/", (req, res) => {
+  res.send("hello world!!!!!!!");
+});
+
+router.get("/api/restaurants", (req, res) => {
   (async () => {
     let testClient;
     try {
@@ -44,10 +48,6 @@ router.get("/", (req, res) => {
       testClient.close();
     }
   })();
-});
-
-router.get("/api/restaurants", (req, res) => {
-  res.send("hello world!!!!!!!");
 });
 
 router.get("/api/restaurants/:rId", (req, resp) => {
