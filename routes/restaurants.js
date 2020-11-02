9a71382a3c102162fe360dc0f91f17aacf87c497
Munawerr <munaweranwar2@gmail.com>
2020-11-02 14:19:26 +0500

    Bugs fixed finally

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -49,7 +49,70 @@ router.get("/api/restaurants", (req, res) => {
 
 router.get("/api/restaurants/:rId", (req, resp) => {
   (async () => {
-    let testClient;
+    let Client;
+    try {
+      testClient = await MongoClient.connect(process.env.MONGODB_URI, {
+        connectTimeoutMS: 200,
+        retryWrites: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+
+      const dataBase = Client.db("sample_restaurants");
+
+      const Restaurants = dataBase.collection("restaurants");
+
+      const restaurant = await Restaurants.findOne({
+        restaurant_id: { $eq: req.params.rId },
+      });
+      resp.send(restaurant);
+    } catch (e) {
+      console.error(e);
+    } finally {
+      Client.close();
+    }
+  })().catch((error) => {
+    res.send(`An Error Occured \n\n${error}`);
+  });
+});
+
+router.post("/api/restaurants", jsonParser, (req, resp) => {
+  const rest = new RestModal(
+    req.body.address,
+    req.body.borough,
+    req.body.cuisine,
+    req.body.grades,
+    req.body.name,
+    req.body.restaurant_id
+  );
+  (async () => {
+    try {
+      testClient = await MongoClient.connect(process.env.MONGODB_URI, {
+        connectTimeoutMS: 200,
+        retryWrites: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+
+      const dataBase = testClient.db("sample_restaurants");
+
+      const Restaurants = dataBase.collection("restaurants");
+
+      await Restaurants.insertOne(rest);
+      resp.send("Restaurant added successfully!");
+    } catch (e) {
+      console.error(e);
+    } finally {
+      testClient.close();
+    }
+  })().catch((error) => {
+    res.send(`An Error Occured \n\n${error}`);
+  });
+});
+
+router.put("/api/restaurants/:rId", jsonParser, (req, resp) => {
+  (async () => {
+    // get Restaurant by Id
     try {
       testClient = await MongoClient.connect(process.env.MONGODB_URI, {
         connectTimeoutMS: 200,
@@ -65,129 +128,79 @@ router.get("/api/restaurants/:rId", (req, resp) => {
       const restaurant = await Restaurants.findOne({
         restaurant_id: { $eq: req.params.rId },
       });
-      resp.send(restaurant);
+
+      if (!restaurant || restaurant == null || restaurant == undefined) {
+        return resp
+          .status(404)
+          .send("The Restaurant with the given ID was not found");
+      }
+
+      // Validate The new Restaurant Data
+      const boroughValid = schema.validate({ Borough: req.body.borough });
+      const cuisineValid = schema.validate({ Cuisine: req.body.cuisine });
+      const nameValid = schema.validate({
+        Name: req.body.name,
+      });
+      if (boroughValid.error) {
+        return resp.status(400).send(boroughValid.error.details[0].message);
+      } else if (cuisineValid.error) {
+        return resp.status(400).send(cuisineValid.error.details[0].message);
+      } else if (nameValid.error) {
+        return resp.status(400).send(nameValid.error.details[0].message);
+      }
+
+      await Restaurants.updateOne(
+        {
+          restaurant_id: { $eq: req.params.rId },
+        },
+        {
+          $set: {
+            borough: req.body.borough,
+            cuisine: req.body.cuisine,
+            name: req.body.name,
+          },
+        }
+      );
+
+      resp.send("Restaurant Updated Successfully!");
     } catch (e) {
       console.error(e);
     } finally {
       testClient.close();
     }
-  })();
+  })().catch((error) => {
+    resp.send(`An Error Occured \n\n${error}`);
+  });
 });
 
-router.post("/api/restaurants", jsonParser, async (req, resp) => {
-  const rest = new RestModal(
-    req.body.address,
-    req.body.borough,
-    req.body.cuisine,
-    req.body.grades,
-    req.body.name,
-    req.body.restaurant_id
-  );
+router.delete("/api/restaurants/:rId", jsonParser, (req, resp) => {
+  (async () => {
+    // get Restaurant by Id
+    try {
+      testClient = await MongoClient.connect(process.env.MONGODB_URI, {
+        connectTimeoutMS: 200,
+        retryWrites: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
 
-  try {
-    testClient = await MongoClient.connect(process.env.MONGODB_URI, {
-      connectTimeoutMS: 200,
-      retryWrites: true,
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+      const dataBase = testClient.db("sample_restaurants");
 
-    const dataBase = testClient.db("sample_restaurants");
+      const Restaurants = dataBase.collection("restaurants");
 
-    const Restaurants = dataBase.collection("restaurants");
-
-    await Restaurants.insertOne(rest);
-    resp.send("Restaurant added successfully!");
-  } catch (e) {
-    console.error(e);
-  } finally {
-    testClient.close();
-  }
-});
-
-router.put("/api/restaurants/:rId", jsonParser, async (req, resp) => {
-  // get Restaurant by Id
-  try {
-    testClient = await MongoClient.connect(process.env.MONGODB_URI, {
-      connectTimeoutMS: 200,
-      retryWrites: true,
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    const dataBase = testClient.db("sample_restaurants");
-
-    const Restaurants = dataBase.collection("restaurants");
-
-    const restaurant = await Restaurants.findOne({
-      restaurant_id: { $eq: req.params.rId },
-    });
-
-    if (!restaurant || restaurant == null || restaurant == undefined) {
-      return resp
-        .status(404)
-        .send("The Restaurant with the given ID was not found");
-    }
-
-    // Validate The new Restaurant Data
-    const boroughValid = schema.validate({ Borough: req.body.borough });
-    const cuisineValid = schema.validate({ Cuisine: req.body.cuisine });
-    const nameValid = schema.validate({
-      Name: req.body.name,
-    });
-    if (boroughValid.error) {
-      return resp.status(400).send(boroughValid.error.details[0].message);
-    } else if (cuisineValid.error) {
-      return resp.status(400).send(cuisineValid.error.details[0].message);
-    } else if (nameValid.error) {
-      return resp.status(400).send(nameValid.error.details[0].message);
-    }
-
-    await Restaurants.updateOne(
-      {
+      await Restaurants.deleteOne({
         restaurant_id: { $eq: req.params.rId },
-      },
-      {
-        $set: {
-          borough: req.body.borough,
-          cuisine: req.body.cuisine,
-          name: req.body.name,
-        },
-      }
-    );
+      });
 
-    resp.send("Restaurant Updated Successfully!");
-  } catch (e) {
-    console.error(e);
-  } finally {
-    testClient.close();
-  }
-});
-
-router.delete("/api/restaurants/:rId", jsonParser, async (req, resp) => {
-  // get Restaurant by Id
-  try {
-    testClient = await MongoClient.connect(process.env.MONGODB_URI, {
-      connectTimeoutMS: 200,
-      retryWrites: true,
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    const dataBase = testClient.db("sample_restaurants");
-
-    const Restaurants = dataBase.collection("restaurants");
-
-    await Restaurants.deleteOne({
-      restaurant_id: { $eq: req.params.rId },
-    });
-
-    resp.send("Restaurant Deleted Successfully!");
-  } catch (e) {
-    console.error(e);
-  } finally {
-    testClient.close();
-  }
+      resp.send("Restaurant Deleted Successfully!");
+    } catch (e) {
+      console.error(e);
+    } finally {
+      testClient.close();
+    }
+  })().catch(() => {
+    resp.send(`An Error Occured \n\n${error}`);
+  });
 });
 
 module.exports = router;
